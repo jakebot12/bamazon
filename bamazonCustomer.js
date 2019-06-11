82b2c54305823f1f9c3091f52bea58699cd14a3f
Jake Gurley <jakemg12@gmail.com>
2019-06-11 12:11:52 -0700

    <Finally got Bamazon up and running properly>

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,31 +1,110 @@
 //initialize dependencies and global variables
+var mysql = require("mysql");
 var inquirer = require("inquirer");
-//write inquirer functions for UI
+require("console.table");
+
+//Set up server connection with database and display products
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon"
+});
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  }
+  getProducts();
+});
+
+//Create welcome message and write inquirer functions for UI
 console.log("Welcome to Bamazon!");
 
-var questions = [
-  {
-    type: "list",
-    name: "shoppingList",
-    message: "Please enter the ID of the the pruduct you would like to find",
-    choices: ["1=HDTV", "2=FishingNet", "3=Hoodie"]
-  },
-  {
-    type: "input",
-    name: "quantity",
-    message: "How many would you like?",
-    validate: function(value) {
-      var valid = !isNaN(parseFloat(value));
-      return valid || "Please enter a number";
-    },
-    filter: Number
-  }
-];
+//Display the inventory in a console table
+function getProducts() {
+  // Query data from products mysql
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    itemPrompt(res);
+  });
+}
 
-inquirer.prompt(questions).then(answers => {
-  console.log("\nOrder receipt:");
-  console.log(JSON.stringify(answers, null, "  "));
-  console.log("Thank you for shopping with Bamazon!")
-});
-//assign the inputs to the proper forms
-//write functions for output
+//Ask the user for their selection
+function itemPrompt(inventory) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "choices",
+        message:"Please enter the ID of the the product you would like to purchase"
+      }
+    ])
+    .then(function(val) {
+      var itemId = parseInt(val.choices);
+      var item = queryInventory(itemId, inventory);
+
+      if (item) {
+        quantityPrompt(item);
+      } else {
+        console.log(
+          "I'm sorry, it looks as though that item has either been sold out, or does not have that quantity available. Please select less quantity or choose another item."
+        );
+        getProducts();
+      }
+    });
+}
+
+//Ask the user how many of their selection they would like to purchase
+function quantityPrompt(item) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many of this item would you like?"
+      }
+    ])
+    .then(function(val) {
+      var quantity = parseInt(val.quantity);
+
+      if (quantity > item.stock_quantity) {
+        console.log(
+          "I'm sorry, it looks as though that item has either been sold out, or does not have that quantity available. Please select less quantity or choose another item."
+        );
+        getProducts();
+      } else {
+        completePurchase(item, quantity);
+      }
+    });
+}
+
+//Purchase Function
+
+function completePurchase(item, quantity) {
+  connection.query(
+    "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+    [quantity, item.itemId],
+    function(err, res) {
+      console.log(
+        "You have completed this purchase of " +
+          quantity +
+          " " +
+          item.product_name +
+          "'s!"
+      );
+      getProducts();
+    }
+  );
+}
+
+//Check the availability of customers' selection
+function queryInventory(itemId, inventory) {
+  for (var i = 0; i < inventory.length; i++) {
+    if (inventory[i].item_id === itemId) {
+      return inventory[i];
+    }
+  }
+  return null;
+}
